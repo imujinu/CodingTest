be9a517f5eb1e45ab9616decd8f62d4e6e933e63
imujinu <wlsdnrhdwkd@gmail.com>
2025-04-28 23:54:29 +0900

    barking

---
+++ src/main/java/com/jw/java/board/Barking4.java
@@ -0,0 +1,18 @@
+package com.jw.java.board;
+
+public class Barking4 {
+    public static void main(String[] args) {
+    int result1 = func4(5);
+    int result2= func4(97615282);
+    int result3= func4(1024);
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+    }
+
+    public static int func4(int num){
+        int val = 1;
+        while(2*val <= num) val*=2;
+        return val;
+    }
+}
