61fde87abede87381e78b8521f64c7c46212e3b2
imujinu <wlsdnrhdwkd@gmail.com>
2025-05-21 18:31:46 +0900

    snail

---
+++ src/main/java/com/jw/java/board/Apt.java
@@ -0,0 +1,24 @@
+package com.jw.java.board;
+
+import java.io.*;
+
+public class Apt {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
+        int number = Integer.parseInt(br.readLine());
+
+        for(int i =0; i<number; i++){
+            int h = Integer.parseInt(br.readLine());
+            int x = Integer.parseInt(br.readLine());
+
+            int[][] apt = new int[h][x];
+
+
+        }
+
+
+
+
+    }
+}
