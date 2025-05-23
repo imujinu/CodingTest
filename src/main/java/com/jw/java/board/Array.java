aed16eaf8a7ff342c44955448bac96d3371cb2ea
imujinu <wlsdnrhdwkd@gmail.com>
2025-05-23 12:40:00 +0900

    array

---
+++ src/main/java/com/jw/java/board/Array.java
@@ -0,0 +1,33 @@
+package com.jw.java.board;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Array {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
+        int range = Integer.parseInt(br.readLine());
+
+        List<Integer> list = new ArrayList<>();
+
+        for(int i=0; i<range; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(list);
+
+        for(int number: list){
+            bw.write(number+"\n");
+        }
+        bw.close();
+        br.close();
+
+
+    }
+}
