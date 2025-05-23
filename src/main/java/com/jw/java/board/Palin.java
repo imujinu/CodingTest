21c4fabbc472b339e18104816ec1ec3dc40e6fea
imujinu <wlsdnrhdwkd@gmail.com>
2025-05-23 23:07:48 +0900

    merge

--- src/main/java/com/jw/java/board/Palin.java
+++ src/main/java/com/jw/java/board/Palin.java
@@ -1,7 +1,33 @@
 package com.jw.java.board;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
 public class Palin {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i =0; i<br.readLine().length(); i++)
+        {
+            StringTokenizer st= new StringTokenizer(br.readLine());
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        while (true){
+
+            ArrayList<Integer> reversedList = new ArrayList<>(list);
+            Collections.reverse(reversedList);
+
+            if(list.get(0).equals(0)) break;
+
+            if (list.equals(reversedList)) {
+               bw.write("yes"+"\n");
+            }else bw.write("wrong"+"\n");
+        }
+        bw.close();
+        br.close();
 
     }
 }
