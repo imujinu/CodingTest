cb05e28b2976ce4e826179dd881d6b9268f8d989
imujinu <wlsdnrhdwkd@gmail.com>
2025-05-25 20:25:08 +0900

    virus

--- src/main/java/com/jw/java/board/now/Virus.java
+++ src/main/java/com/jw/java/board/now/Virus.java
@@ -1,19 +1,44 @@
 package com.jw.java.board.now;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Virus {
     static boolean[][] graph;
     static boolean[] visited;
     static int N, M;
     static int answer;
-    
-    public static void main(String[] args) {
+
+    public static void dfs(int idx){
+        answer++;
+        visited[idx]=true;
+        for(int i =1; i<=N; i++){
+            if(!visited[i]&& graph[idx][i]){
+                dfs(i);
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        graph = new boolean[N+1][N+1];
+        visited = new boolean[N+1];
+        int x, y;
+        for(int i =0; i<M ; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+            graph[x][y] = graph[y][x] = true;
+        }
+
+        dfs(1);
+        bw.write(String.valueOf(answer-1));
+        bw.close();
+        br.close();
 
 
     }
