ab52fbc782a95fb1a069444e8c2171584f3bd7b5
imujinu <wlsdnrhdwkd@gmail.com>
2025-05-27 18:31:38 +0900

    printerqueue

---
+++ src/main/java/com/jw/java/board/MayTwentySeven/Line.java
@@ -0,0 +1,14 @@
+package com.jw.java.board.MayTwentySeven;
+
+import java.io.*;
+
+public class Line {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+    }
+}
