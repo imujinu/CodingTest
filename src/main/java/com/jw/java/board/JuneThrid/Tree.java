adccc990373bee1953722a0ff699f208ed07fa8f
imujinu <wlsdnrhdwkd@gmail.com>
2025-06-08 19:26:49 +0900

    tree

---
+++ src/main/java/com/jw/java/board/JuneThrid/Tree.java
@@ -0,0 +1,11 @@
+package com.jw.java.board.JuneThrid;
+
+import java.io.*;
+
+public class Tree {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    }
+}
