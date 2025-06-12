b9ca58b559ca42d4c408550a8f23e5b9c25850eb
imujinu <wlsdnrhdwkd@gmail.com>
2025-06-12 15:47:21 +0900

    pq

---
+++ src/main/java/com/jw/java/board/JuneTwelve/Pq.java
@@ -0,0 +1,26 @@
+package com.jw.java.board.JuneTwelve;
+
+import java.io.*;
+import java.util.PriorityQueue;
+
+public class Pq {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0; i<N; i++){
+            int M = Integer.parseInt(br.readLine());
+            if(M==0){
+                if(pq.isEmpty())bw.write("0\n");
+                else{
+                    bw.write(String.valueOf(pq.poll())+"\n");
+                }
+            }
+            else{
+                pq.add(M);
+            }
+        }
+        bw.close();
+    }
+}
