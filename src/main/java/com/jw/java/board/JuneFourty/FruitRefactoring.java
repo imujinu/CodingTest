6df408ac79c6bb1896deeaeb2181b618f4200a17
imujinu <wlsdnrhdwkd@gmail.com>
2025-06-15 21:29:58 +0900

    minecraft

---
+++ src/main/java/com/jw/java/board/JuneFourty/FruitRefactoring.java
@@ -0,0 +1,12 @@
+package com.jw.java.board.JuneFourty;
+
+import java.io.*;
+
+public class FruitRefactoring {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+
+    }
+}
--- src/main/java/com/jw/java/board/JuneFourty/MineCraft.java
+++ src/main/java/com/jw/java/board/JuneFourty/MineCraft.java
@@ -1,6 +1,7 @@
 package com.jw.java.board.JuneFourty;
 
 import java.io.*;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class MineCraft {
@@ -19,36 +20,41 @@ public class MineCraft {
             for(int j=0; j<M; j++){
                 int num = Integer.parseInt(st.nextToken());
                 ground[i][j] = num;
-                max = Math.max(max, num);
-                min = Math.min(min,num);
+                if(num>max) max =num;
+                if(num<min) min = num;
+            }}
+        int minCount =Integer.MAX_VALUE;
+        int hegiht = 0;
+        for(int i=min; i<=max; i++){
+                int num = i;
+                int count =0;
+                int bag = K;
+            for(int j=0; j<N; j++){
+                for(int k=0; k<M; k++){
+                    int block = ground[j][k];
+
+                    if(block>num){
+                        count+=((block-num)*2);
+                        bag+=(block-num);
+                    }
+                    else if(block<num){
+                            count+=num-block;
+                            bag-=(num-block);
+
+                    }
+                }
             }
+                if(bag<0)break ;
+
+
+                if(minCount>=count){
+                    minCount=count;
+                    hegiht=i;
+                }
+
         }
-        int maxCount = 0;
-        int minCount = 0;
-            for(int[] nums : ground){
-                for(int num : nums){
-                    if(num!=max){
-                        maxCount += max-num;
-                    }
-                }
-            }
-
-            for(int[] nums: ground){
-                for (int num: nums){
-                    if(num!=min){
-                        minCount+= (num-min)*2;
-                    }
-                }
-            }
-
-            if(maxCount<minCount && maxCount<K){
-                bw.write(String.valueOf(maxCount)+" "+String.valueOf(max));
-            }else{
-                bw.write(String.valueOf(minCount)+" "+String.valueOf(min));
-            }
-            bw.close();
-
-
+        bw.write(String.valueOf(minCount)+" "+String.valueOf(hegiht));
+        bw.close();
 
 
 
