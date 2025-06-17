1d964e9c22519254415018165315a0940301dbd0
imujinu <wlsdnrhdwkd@gmail.com>
2025-06-17 21:47:03 +0900

    snake

--- src/main/java/com/jw/java/board/JuneSeventeen/Snake.java
+++ src/main/java/com/jw/java/board/JuneSeventeen/Snake.java
@@ -1,45 +1,76 @@
 package com.jw.java.board.JuneSeventeen;
 
 import java.io.*;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Snake {
+    static int[] map;
+    static boolean[] visited;
+    static HashMap<Integer,Integer> ladder;
+    static int minCount ;
+    static void dfs(int start, int count){
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{start,count});
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int[] arr = queue.poll();
+            int n = arr[0];
+            int cnt = arr[1];
+            if(n==100){
+                minCount =cnt;
+                return;
+            }
+
+            for (int i = 1; i < 7; i++) {
+                int next = n+i;
+                if (next > 100) continue;
+                if(ladder.containsKey(next)){
+                    next = ladder.get(next);
+                }
+
+                if(!visited[next]){
+                    visited[next] = true;
+
+                queue.add(new int[]{next, cnt + 1});
+                }
+
+
+            }
+
+        }
+
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int start = 0;
-        int end= Integer.MIN_VALUE;
+        map = new int[101];
+        for(int i=1; i<=100; i++){
+            map[i]=i;
+        }
+
+        visited = new boolean[101];
+        ladder =new HashMap<>();
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
             int num1 = Integer.parseInt(st.nextToken());
             int num2 = Integer.parseInt(st.nextToken());
-            if(num2>end){
-                end=num2;
-                start=num1;
-            }
+            ladder.put(num1,num2);
         }
 
-        Set<Integer> set = new HashSet<>();
+
         for(int i=0; i<M; i++){
-            st = new StringTokenizer(br.readLine());
+            st=  new StringTokenizer(br.readLine());
             int num1 = Integer.parseInt(st.nextToken());
-            int num2 = Integer.parseInt(st.nextToken());
-            set.add(num1);
-        }
-        int count = 0;
-
-        count+= (int)Math.ceil((double) (100-end)/6);
-
-        while(start<=0){
-            start-=6;
-
+            visited[num1] = false;
         }
 
+        dfs(1,0);
+        bw.write(String.valueOf(minCount));
+        bw.close();
 
 
     }
