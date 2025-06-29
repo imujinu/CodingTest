61553f26f3c34eb8670da8a7e737517b121130a4
imujinu <wlsdnrhdwkd@gmail.com>
2025-06-29 23:49:04 +0900

    city

--- src/main/java/com/jw/java/board/June/TwentySeven/City.java
+++ src/main/java/com/jw/java/board/June/TwentySeven/City.java
@@ -10,10 +10,10 @@ public class City {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        List<int[]> list = new ArrayList<>();
         int[] cost = new int[M];
         int[] visit = new int[M];
-        int[] dp = new int[N+1];
+        int[] dp = new int[2501];
+        Arrays.fill(dp, Integer.MAX_VALUE);
         for (int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine());
             int temp1 = Integer.parseInt(st.nextToken());
@@ -21,16 +21,26 @@ public class City {
             cost[i] = temp1;
             visit[i] = temp2;
         }
-
+        dp[0] = 0;
         for(int i=0; i<M; i++){
-            for(int j=N; j>=N-cost[i]; j--){
-                dp[j]=
+            for(int j=visit[i]; j<dp.length ; j++){
+                if(dp[j-visit[i]]!=Integer.MAX_VALUE){
+
+                dp[j]=Math.min(dp[j-visit[i]]+cost[i],dp[j]);
+                }
             }
         }
+        //i == 1  dp[5] dp[6] dp [7] ... dp[12]
+        //dp는 N명을 유치하는데 들어간느 최소 비용
+        // 아 그러면 사실 13명을 유치하는데 비용이 더 작을 수도 있음.
+        int answer = Integer.MAX_VALUE;
 
-        int[] arr = new int[N+1];
+        for(int i=N; i<dp.length; i++){
+            answer = Math.min(answer,dp[i]);
+        }
 
-
+        bw.write(String.valueOf(answer));
+        bw.close();
 
     }
 }
