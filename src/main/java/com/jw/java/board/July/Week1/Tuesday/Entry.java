e0b92769666da5a0dc10409ef807e89bee105708
imujinu <wlsdnrhdwkd@gmail.com>
2025-07-03 23:36:12 +0900

    entry

---
+++ src/main/java/com/jw/java/board/July/Week1/Tuesday/Entry.java
@@ -0,0 +1,45 @@
+package com.jw.java.board.July.Week1.Tuesday;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Entry {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        long[] arr = new long[N];
+
+        for(int i=0; i<N; i++){
+            arr[i] = Long.parseLong(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        long left=1;
+        long right = arr[arr.length-1]*M;
+        long result = Long.MAX_VALUE;
+        while(left<right){
+            long mid = (left+right)/2;
+            long temp = 0;
+            for(long i : arr){
+                temp+= mid/i;
+            }
+
+
+
+            if(temp>=M){
+                right= mid;
+            }
+            else{
+
+                left=mid+1;
+            }
+
+        }
+        bw.write(String.valueOf(left));
+        bw.close();
+    }
+}
