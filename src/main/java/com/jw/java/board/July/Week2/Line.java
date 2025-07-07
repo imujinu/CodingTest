40e449c319496aa532c536be1d0eff60af113028
imujinu <wlsdnrhdwkd@gmail.com>
2025-07-07 22:50:08 +0900

    line

--- src/main/java/com/jw/java/board/July/Week2/Line.java
+++ src/main/java/com/jw/java/board/July/Week2/Line.java
@@ -1,7 +1,58 @@
 package com.jw.java.board.July.Week2;
 
+import java.io.*;
+import java.util.*;
+
 public class Line {
-    public static void main(String[] args) {
+    static int N,M;
+    static List<List<Integer>> list;
+    static StringBuilder sb;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N+1];
+        list = new ArrayList<>();
+        sb = new StringBuilder();
+        for(int i=0; i<=N; i++){
+            list.add(new ArrayList<>());
+        }
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            list.get(start).add(end);
+            arr[end]++;
+        }
+
+
+        topological();
+        System.out.println(sb);
+
+    }
+    static void topological(){
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=1; i<=N; i++ ){
+            if(arr[i]==0){
+
+                queue.add(i);
+            }
+        }
+
+        while (!queue.isEmpty()){
+            int temp = queue.poll();
+            sb.append(temp).append(" ");
+            for(int next : list.get(temp)){
+                arr[next]--;
+
+                if(arr[next]==0){
+                    queue.add(next);
+                }
+            }
+        }
 
     }
 }
