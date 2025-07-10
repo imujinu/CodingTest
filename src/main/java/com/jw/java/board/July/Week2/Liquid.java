b362ebccedec42bedd6e2dc1ff9cb2189f3442c4
imujinu <wlsdnrhdwkd@gmail.com>
2025-07-10 10:29:12 +0900

    liquid

--- src/main/java/com/jw/java/board/July/Week2/Liquid.java
+++ src/main/java/com/jw/java/board/July/Week2/Liquid.java
@@ -8,44 +8,44 @@ public class Liquid {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        long[] arr = new long[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
-        int left =0;
-        int right = arr.length-1;
-        long min = Long.MAX_VALUE;
-        long[] result = new long[3];
+        long [] result = new long[3];
+        long temp = Long.MAX_VALUE;
+        for(int i=0; i<N-2; i++){
+            int left = i+1;
+            int right = arr.length-1;
 
-        while(left<right){
-            long sum = 0;
-            for(int i=left+1; i<right; i++){
-                int temp =arr[left] + arr[i] + arr[right];
-                if(Math.abs(temp)<min){
-                    min = Math.abs(temp);
-                    sum = temp;
-                    result[0] = arr[left];
-                    result[1] = arr[i];
+            while(left<right){
+            long mid = arr[i] + arr[left] + arr[right];
+                if(temp>Math.abs(mid)){
+                    temp = Math.abs(mid);
+                    result[0] =arr[i];
+                    result[1] =arr[left];
                     result[2] = arr[right];
                 }
-                if(temp==0) break;
-            }
 
-            if(sum<0){
-                left++;
-            }else{
-                right--;
-            }
+                if(mid==0)break;
 
+               if(mid>0){
+                   right--;
+               }else{
+                   left++;
+               }
+
+
+            }
         }
 
-        for(long i: result){
-            bw.write(String.valueOf(i)+" ");
-        }
-
-        bw.close();
+            Arrays.sort(result);
+            for(long i : result){
+                bw.write(String.valueOf(i)+" ");
+            }
+            bw.close();
     }
 }
